8789eaf600027cc952c5565102561faaf2ea1d29
Karine Grassi <karine-felicia.grassi@ac-normandie.fr>
2020-03-02 19:45:04 +0100

    menu hambuger

--- appk.js
+++ appk.js
@@ -1,5 +1,18 @@
-var content = document.getElementById ('menu-content').innerHTML;
-var sidebarBody = document.getElementById ('menu-sidebar-body');
+let content = document.getElementById('menu-content');
+let sidebarBody = document.getElementById('menu-sidebar-body');
 
-console.log(content);
-sidebarBody.innerHTML += content;
+let button = document.getElementById('menu-button');
+let overlay = document.getElementById('menu-overlay');
+let activatedClass = 'menu-activated';
+
+sidebarBody.innerHTML += content.innerHTML;
+
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+    this.parentNode.classList.add(activatedClass);
+});
+
+overlay.addEventListener('click', function(e) {
+    e.preventDefault();
+   this.parentNode.classList.remove(activatedClass);
+});
