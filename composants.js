ec8de469b2fa1447b3a26d817aa7774c7935021a
Karine Grassi <karine-felicia.grassi@ac-normandie.fr>
2020-03-20 09:03:00 +0100

    modif menu DRY

--- composants.js
+++ composants.js
@@ -5,14 +5,15 @@ class Burger{
     target.appendChild(this.dom);
     this.content=[];
     window.mvp.burger = this;
-    this.dom.onclick = this.changeAffichage.bind(this);
+    this.menu = document.getElementById('menu-button');
+    this.menu.onclick = this.changeAffichage.bind(this);
     this.minified = true;
     this.render();
   }
 
-  render(){
-    if (this.minified) return this.renderMinified();
-    this.renderUnMinified();
+  render(){//si minified est vraie alors on affiche le petit burger, sinon le burger est déplié
+    if (this.minified) return this.renderSmallBurger();
+    this.renderBurger();
   }
  
   changeAffichage(){
@@ -20,15 +21,13 @@ class Burger{
     this.render();
   }
 
-  renderMinified(){
-    this.dom.innerHTML = `<div id="menu-button" class="accordion-toggle"><i class="fas fa-bars"></i></div>`;
+  renderSmallBurger(){
+    this.dom.innerHTML = `<div></div>`;
     this.dom.className = "smallburger";
   }
 
-  renderUnMinified(){
+  renderBurger(){
     this.dom.innerHTML = `
-      	<div id="menu-button" class="accordion-toggle"><i class="fas fa-bars"></i></div>
-
       	<ul class="accordion-inner"> 
 			<li><a  href="index.html"><i class="fas fa-home"></i> Accueil</a></li>
 			<li><a href="panier.html"><i class="fas fa-shopping-cart"></i> Panier</a></li>
@@ -83,15 +82,11 @@ class Produit{
 class ListeProduits{
 	constructor(target){
 		this.liste = [];
-
 		this.DOM = document.createElement("div");
 		target.appendChild(this.DOM);
-		//this.affichageAccueil();
 	}
 
 	ajouteProduit(produit) {
-		//this.DOM = document.createElement("div");
-		//console.log("test");
 		this.liste.push(produit);
 	}
 	
@@ -126,12 +121,12 @@ class Meuble{
 		for (let i=0; i<this.produit.vernis.length; i++) {
 			vernis += `<option value="${this.produit.vernis[i]}">${this.produit.vernis[i]}</option>`;
 		}
+		//let quantity = "";
 		this.DOM.innerHTML = `
 			<img src="${this.produit.image}" alt="${this.produit.nameLong}"/>
 			<div id="${this.produit.name}content">
 				<figcaption>${this.produit.nameLong}</figcaption>
 				<p>${this.produit.description}</p>
-				
 				<select id="mon_vernis">
 					<option selected>-- Choix du vernis --</option>
 					${vernis}
@@ -144,30 +139,64 @@ class Meuble{
 				</a>
 			</div>		
 		`;
-		
 	}
 }
-
+// récupération des objets "meubles" pour les ajouter au panier
 class Panier {
-	constructor(target){
+	constructor(target){ // cible dans laquelle on va implémenter les meubles
 		this.commande = [];
+		/*this.trash = trash;
+		document.getElementById("trash-${produit.id}");
+		console.log(trash);
+		this.trash.onclick = this.supprimeProduit.bind(this);*/
+				
 		window.mvp.panier = this;
 		this.DOM = document.createElement("tbody");
 		target.appendChild(this.DOM);
+		this.recupPanier();
+		this.supprimerPanier();
+	}
+
+	recupPanier() {
+		//localStorage.clear(); //me permet de vider le panier
+		for(let i=0; i<localStorage.length; i++) {
+			let key = localStorage.key(i);
+			let meubleJson = localStorage.getItem(key);
+			let meubleJs = JSON.parse(meubleJson);
+			this.ajouteProduit(meubleJs);
+		}
+	}
+
+	estPresent(produit) {
+		for(let meuble of this.commande) {
+			if(meuble.id === produit.id){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	ajouteProduit(produit){
-		this.commande.push(produit);
+		if (!this.estPresent(produit)) {
+			this.commande.push(produit);
+			localStorage.setItem(produit.id,JSON.stringify(produit));
+			//localStorage.setItem("panier",JSON.stringify(commande));
+		}
 	}
 
+	/*supprimeProduit(produit){
+			this.commande.
+			localStorage.removeItem(produit.id);
+	}*/
+
 	affichagePanier() {
-		console.log(this.commande);
 		for (let produit of this.commande) {
 			this.DOM.innerHTML += `
 			<tr>
 				<td><img src="${produit.image}" alt="${produit.nameLong}"/></td>
 				<td>${produit.nameLong}</td>
 				<td>${produit.price}€</td>
+				<td id="trash-${produit.id}" ><i class="far fa-trash-alt"></i></td>
 			</tr>
 		`;	
 		}
@@ -179,6 +208,14 @@ class Panier {
 		`;
 	}
 
+	supprimerPanier() {
+		document.getElementById("delete").addEventListener('click', ()=>{
+			localStorage.clear(); 
+			window.location.reload();
+			console.log(localStorage);
+		})
+	}
+
 	totalPanier() {
 		let total = 0;
 		for(let produit of this.commande) {
@@ -188,63 +225,39 @@ class Panier {
 	}
 }
 
-//page PANIER
-/*class Panier {
-	constructor(data, target){
-		//this.name = data.name.split(" ")[0];
-		this.nameLong = data.name;
-		this.price = data.price/100;
-		this.description = data.description;
-		this.image = data.imageUrl;
-		this.content = [];
-		this.contenuPanier = [];
-		//this.total = this.price++;
-
-		window.mvp.panier = this;
-		this.DOM = document.createElement("tbody");
-		target.appendChild(this.DOM);
-
-		this.affichageLignePanier();
-		//this.affichageTotalPanier();
-		//this.affichagePanier()
-	}
-		
-	//affichage du tableau de produits sélectionnés (pour le moment un par un : pas de liste)
-	/*async function affichageLignePanier(){ 
-		this.DOM.innerHTML = `
-			<tr>
-				<td><img src="${this.image}" alt="${this.nameLong}"/></td>
-				<td>${this.nameLong}</td>
-				<td>${this.price}€</td>
-			</tr>
-		`;		
-	}
-	/*async function affichageTotalPanier(){
-		this.DOM.innerHTML = `
-			<tr id="total">
-				<td colspan="2"> Total :</td>
-				<td>${this.total}€</td>
-			</tr>
-		`;
-	}
-
-	affichagePanier(){
-		const ligneCommande = await affichageLignePanier();
-		const totalCommande = await affichageTotalPanier();
-		return ligneCommande + totalCommande;
-	}*/
-
-//}
-/*class Contact {
+//
+class Contact {
 	constructor(){
-		this.firstName = data.firstName;
-		this.lastName = data.lastName;
-		this.address = data.address;
-		this.city = data.city;
-		this.email = data.email;
+		this.contact = {};
+		this.firstName = sessionStorage.getItem("prenom");
+		this.lastName = "";
+		this.address = "";
+		this.city = "";
+		this.email = "";
+		window.mvp.contact = this;
 
-		 window.mvp.contact = this;
+		this.saveContact();
 	}
+	/*recupContact() {
+		let key = sessionStorage.key(i);
+		let contactJson = sessionStorage.getItem(key);
+		
+	}*/
+	saveContact() {
+		if(typeof sessionStorage!='undefined') {
+  			if('message' in sessionStorage) {
+    		alert("Message récupéré");
+    		document.getElementById("firstName").value = sessionStorage.getItem('message');
+ 			 }
+		} else {
+		  alert("sessionStorage n'est pas supporté");
+		}
+	}
+	
 
+}
 
-}*/
+/* 
+//
+
+*/
